0d792a569f44b83fab1a590349582e30f44e3098
eduardoanaya <gran_ed@hotmail.com>
2017-04-07 02:13:19 +0200

    enabled google searches :)

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,13 @@
 const Botkit = require('botkit');
-let bot;
+const google = require('google');
+const R = require('ramda');
+
+const baseOpts = {
+  resultsPerPage: 3
+};
 function start(token) {
   const botController = Botkit.slackbot({ debug: false });
-  bot = botController.spawn({ token });
+  const bot = botController.spawn({ token });
 
   bot.startRTM((err, bot, payload) => {
     if (err) {
@@ -10,8 +15,49 @@ function start(token) {
     }
   });
   botController.hears(['hello','hi'],['direct_message','direct_mention','mention'],function(bot,message) {
-    bot.reply(message,"Hello.");
+    bot.reply(message, 'Hello! :)');
   });
+  botController.hears(
+    ['search'],
+    ['direct_message','direct_mention','mention'],
+    (bot, message) => {
+      bot.reply(message, 'HALLO! here are your results... enjoy!');
+      foodRecomendations(message, (err, result) => {
+        if(err) {
+          console.log(err);
+          bot.reply(message, `O no! there was an error: ${err}`);
+        }
+        R.map(e => {
+          bot.reply(message, `:ice_cream_parrot: ${e.link}`)
+        }, result);
+      });
+    }
+  );
+}
+
+function foodRecomendations(message, callback) {
+  const query = R.replace(R.toLower(message.match[0]),'', message.text);
+  console.log('query', query);
+  googleSearch(query, baseOpts, (err, res) => {
+    if(err) {
+      callback(err);
+      return;
+    }
+    callback(null, res);
+  });
+}
+
+
+function googleSearch(text, opts, callback) {
+  google.resultsPerPage = opts.resultsPerPage;
+  google.protocol = 'http';
+  google(text, (err, response) => {
+    if(err) {
+      callback(new Error(err));
+      return;
+    }
+    callback(null, response.links);
+  })
 }
 
 module.exports = { start };
